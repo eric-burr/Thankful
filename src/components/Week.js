18dd4b197f6a131ef451d11b15a99cbd646b98b3
Eric Burr <eric.burr91@gmail.com>
2021-03-27 16:24:11 -0600

    finally dynamic first day of month!!

--- src/components/Week.js
+++ src/components/Week.js
@@ -5,26 +5,23 @@ export default function Week(props) {
     var date = new Date();
     var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
     var firstdayofmonth = firstDay.getDay();
-
-    useEffect(() => {
-        for(let i=0;i<=30;i++){
-            if(firstdayofmonth === props.dates[i]) {
-                newthing(props.theday) 
-            }
-        }
-    })
+   
     
     return (
         <div>
             
             <div className='week'>
-            {props.dates.map((value, index) => (
-                console.log('value', props.dates[firstdayofmonth]),
+            {props.dates.map((value, index) => {
+                //comparing to index since it could only be 0-7
+                //so it'll be on the first line of the calendar
+                return firstdayofmonth===index ?
+                
           <div key={index} className='sunday'>
-              
-          </div>
-          
-            ))}
+              {firstdayofmonth} 
+          </div> 
+          : 
+          <div className='sunday'></div>
+            })}
             </div>
         </div>
     )
